90ed189de2e16a48ae25bd68fe999a85b528ced1
Sebastian Matysiak <sebastian.matysiak@hotmail.com>
2021-02-19 22:39:56 +0000

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+export const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="text-2xl">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic beatae harum repudiandae
+        possimus neque consectetur recusandae vel? Officiis voluptas corporis deleniti, minima id ad
+        minus asperiores soluta qui quae provident enim atque nisi voluptatibus beatae molestiae
+        autem adipisci? Dolorum consequatur dolores possimus repudiandae unde nam assumenda
+        reprehenderit alias! Doloribus, error!
+      </div>
     </div>
   );
-}
-
-export default App;
+};
