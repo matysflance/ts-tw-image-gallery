ad2ca38f7408a6d94ad799bae3a57bf0579bc51e
Sebastian Matysiak <sebastian.matysiak@hotmail.com>
2021-02-21 12:57:49 +0000

    Added Search field and keyword search functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import { ImageCard } from './components/ImageCard';
 import { ImageInterface } from './components/ImageCard';
+import { ImageSearch } from './components/ImageSearch';
 
 export const App: React.FC = () => {
   const [images, setImages] = useState<ImageInterface[]>([]);
-  const [loading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
   const [searchTerm, setSearchTerm] = useState('');
-
+  useEffect(() => {
+    console.log(searchTerm);
+  }, [searchTerm]);
   useEffect(() => {
     fetch(
       `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${searchTerm}&image_type=photo&pretty=true`,
@@ -21,11 +24,20 @@ export const App: React.FC = () => {
 
   return (
     <div className="container mx-auto">
-      <div className="grid grid-cols-3 gap-4">
-        {images.map((image: ImageInterface) => (
-          <ImageCard image={image} key={image.id} />
-        ))}
-      </div>
+      <ImageSearch setSearchTerm={setSearchTerm} />
+      {!isLoading && !images.length && (
+        <h1 className="text-5xl text-center mx-auto mt-32">No images found</h1>
+      )}
+
+      {isLoading ? (
+        <h1 className="text-6xl text-center mx-auto mt-32">Loading...</h1>
+      ) : (
+        <div className="grid grid-cols-3 gap-4">
+          {images.map((image) => (
+            <ImageCard image={image} key={image.id} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
